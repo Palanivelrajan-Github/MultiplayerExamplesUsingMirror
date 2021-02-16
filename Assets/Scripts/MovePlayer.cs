a78fb9f6b6f2692a6b4868347a9b0bdf27d7e5b5
rohan29github <72843486+rohan29github@users.noreply.github.com>
2021-02-16 15:42:35 +0530

    initial commit

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
+    [SerializeField] [Range(1.0f, 100.0f)] public float speed;
+
     // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
-        
+        speed = 10.0f;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var horizontal = Input.GetAxis("Horizontal") * speed;
+        var vertical = Input.GetAxis("Vertical") * speed;
+        transform.Translate(horizontal, 0.0f, vertical);
     }
 }
